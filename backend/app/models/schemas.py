7ad16b5e4753983a0bcd1e7a63eaf9cc57656b98
Rayan15-07 <rayan.hannani@icloud.com>
2026-02-27 11:20:10 -0500

    Update schemas.py

--- backend/app/models/schemas.py
+++ backend/app/models/schemas.py
@@ -1,1 +1,14 @@
+from pydantic import BaseModel
+from typing import List
+
+class NearestNeighbor(baseModel):
+  id: str
+  score: float
+
+class EmbedResponse:
+  embedding_id: str
+  nearest_neighbors: 
+
+
+
 
